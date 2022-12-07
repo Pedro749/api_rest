effda7c0285b58ca727d96058381ff0989f443cc
Pedro Augusto Pereira Florentino <pedropereiraflorent@hotmail.com>
2022-12-07 15:23:21 -0300

    Criando seed

---
+++ src/database/seeds/20221207181032-create-users.js
@@ -0,0 +1,39 @@
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('users', [
+      {
+        name: 'John Doe',
+        email: 'jonh@email.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'John Doe 1',
+        email: 'jonh1@email.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'John Doe 2',
+        email: 'jonh2@email.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'John Doe 3',
+        email: 'jonh3@email.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+
+    ], {});
+  },
+
+  async down() {},
+};
