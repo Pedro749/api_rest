e11cd3580e56d5f858f000623117c0bb33af5c7c
Pedro Augusto Pereira Florentino <pedropereiraflorent@hotmail.com>
2022-12-07 17:19:08 -0300

    Alterações rotas de Students

--- src/models/Student.js
+++ src/models/Student.js
@@ -3,11 +3,55 @@ import Sequelize, { Model } from 'sequelize';
 export default class Student extends Model {
   static init(sequelize) {
     super.init({
-      name: Sequelize.STRING,
-      email: Sequelize.STRING,
-      age: Sequelize.INTEGER,
-      weight: Sequelize.FLOAT,
-      height: Sequelize.FLOAT,
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Name must be between 3 and 255 characters',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email is already being used',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Invalid email',
+          },
+        },
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Age must be integer',
+          },
+        },
+      },
+      weight: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Weight must be a number',
+          },
+        },
+      },
+      height: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Height must be a number',
+          },
+        },
+      },
     }, {
       sequelize,
     });
--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import studentController from '../controllers/StudentController';
+import loginRequired from '../middleware/loginRequired';
 
 const router = new Router();
 
 router.get('/', studentController.index);
+router.get('/:id/', studentController.show);
+router.post('/', loginRequired, studentController.store);
+router.put('/:id/', loginRequired, studentController.update);
+router.delete('/:id/', loginRequired, studentController.delete);
 
 export default router;
