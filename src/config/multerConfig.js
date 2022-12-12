8886e3a5561a5edadaeb4d63e9b42752708a2eb5
Pedro Augusto Pereira Florentino <pedropereiraflorent@hotmail.com>
2022-12-12 16:50:20 -0300

    Filtro upload

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -4,6 +4,12 @@ import { extname, resolve } from 'path';
 const random = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
+  fileFilter: (request, file, callback) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return callback(new multer.MulterError('type not suported'));
+    }
+    return callback(null, true);
+  },
   storage: multer.diskStorage({
     destination: (request, file, callback) => {
       callback(null, resolve(__dirname, '..', '..', 'uploads'));
--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,6 +1,18 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig).single('photo');
+
 class PhotoController {
   async store(request, response) {
-    response.json('ok');
+    return upload(request, response, (error) => {
+      if (error) {
+        return response.status(400).json({
+          error: [error.code],
+        });
+      }
+      return response.json(request.file);
+    });
   }
 }
 
