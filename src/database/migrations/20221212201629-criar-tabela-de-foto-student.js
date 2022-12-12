d957b1a2328aa776864cb72afa60cfe76146991f
Pedro Augusto Pereira Florentino <pedropereiraflorent@hotmail.com>
2022-12-12 17:51:07 -0300

    migration table photos

---
+++ src/database/migrations/20221212201629-criar-tabela-de-foto-student.js
@@ -0,0 +1,44 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('photos', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      originalname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      filename: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      student_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'students',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('photos');
+  },
+};
--- src/routes/photoRoutes.js
+++ src/routes/photoRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import photoController from '../controllers/PhotoController';
+import loginRequired from '../middleware/loginRequired';
 
 const router = new Router();
 
-router.post('/', photoController.store);
+router.post('/', loginRequired, photoController.store);
 
 export default router;
