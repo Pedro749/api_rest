76c0ad6e4cf5d83bb291d4e8f7fe5eef9e129652
Pedro Augusto Pereira Florentino <pedropereiraflorent@hotmail.com>
2022-12-13 08:47:02 -0300

    Listando fotos dos estudantes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,7 +4,7 @@ import loginRequired from '../middleware/loginRequired';
 
 const router = new Router();
 
-router.post('/', userController.store);
+router.post('/', loginRequired, userController.store);
 router.put('/', loginRequired, userController.update);
 router.delete('/', loginRequired, userController.delete);
 
